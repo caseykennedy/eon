7eafd37a9aef761732aa1f62cf79ac4359c9495b
Casey K <tehcnicolour@gmail.com>
2020-10-21 14:45:38 -0700

    button styles

--- web/src/components/AddToCart/styles.scss.tsx
+++ web/src/components/AddToCart/styles.scss.tsx
@@ -21,7 +21,7 @@ export const AddToCart = styled(Flex)`
   align-items: center;
   justify-content: space-between;
 
-  padding: calc(${theme.space[2]} / 1.15) ${theme.space[3]};
+  padding: ${theme.space[3]} ${theme.space[4]};
   /* max-width: 400px; */
   /* width: 100%; */
 
@@ -40,18 +40,31 @@ export const AddToCart = styled(Flex)`
   cursor: pointer;
   outline: none;
   transition: ${theme.transition.all};
+  white-space: nowrap;
 
   @media ${theme.mq.desktop} {
     /* width: calc(${theme.space[7]} * 4); */
     font-size: ${theme.fontSizes[3]};
+    padding: calc(${theme.space[2]} / 1.15) ${theme.space[3]};
   }
 
   span {
-    margin-left: ${theme.space[7]};
+    margin-left: ${theme.space[5]};
     transition: ${theme.transition.all};
 
     position: relative;
     right: ${theme.space[2]};
+
+    display: none;
+
+    @media ${theme.mq.tablet} {
+      display: block;
+      margin-left: ${theme.space[6]};
+    }
+
+    @media ${theme.mq.desktop} {
+      margin-left: ${theme.space[7]};
+    }
 
     svg {
       width: calc(${theme.space[5]});
