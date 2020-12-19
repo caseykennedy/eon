a42b54f69dcb612a1674e56169ee64afa7c9e7e8
Casey K <tehcnicolour@gmail.com>
2020-12-19 12:25:58 -0800

    image prune

--- web/src/components/Layout/Layout.tsx
+++ web/src/components/Layout/Layout.tsx
@@ -54,7 +54,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                 <S.Main ref={mainRef}>{children}</S.Main>
                 <Footer />
               </S.Wrapper>
-              <NewCustomerModal />
             </ParallaxProvider>
           </ThemeProvider>
         </ContextProvider>
